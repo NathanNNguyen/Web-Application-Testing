a3da22718ecb03793bf4bf85876f477443a22597
Nhan <natenguyen51294@gmail.com>
2020-01-16 13:58:54 -0600

    added basic dashboard setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './Components/Dashboard';
-import Display from './Components/Display';
 
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
-      <Display />
     </div>
   );
 }
--- my-app/src/Components/Dashboard.js
+++ my-app/src/Components/Dashboard.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 function Dashboard() {
+
+  const strike = (e) => {
+    e.preventDefault();
+
+  }
+
+  const ball = (e) => {
+    e.preventDefault();
+
+  }
+
+  const foul = (e) => {
+    e.preventDefault();
+    
+  }
+  
+  const hit = (e) => {
+    e.preventDefault();
+    
+  }
+
+
   return (
     <div>
-      Hello Dashboard
+      <button onClick={strike}>Strike</button>
+      <button onClick={ball}>Ball</button>
+      <button onClick={foul}>Foul</button>
+      <button onClick={hit}>Hit</button>
     </div>
   );
 }
