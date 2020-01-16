744d9a91819e3f1997b45962b92440ad1bca8b78
Nhan <natenguyen51294@gmail.com>
2020-01-16 14:38:40 -0600

    added logic to dashboard

--- my-app/src/Components/Dashboard.js
+++ my-app/src/Components/Dashboard.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Dashboard() {
 
-  const strike = (e) => {
-    e.preventDefault();
-
-  }
-
-  const ball = (e) => {
-    e.preventDefault();
-
-  }
-
-  const foul = (e) => {
-    e.preventDefault();
-    
-  }
+  const [strikes, setStrikes] = useState('');
+  const [balls, setBalls] = useState('');
   
-  const hit = (e) => {
+  const ballHandle = (e) => {
     e.preventDefault();
-    
+    if(balls < 4) {
+      setBalls(balls + 1)
+    } 
+    else {
+      setBalls(0)
+    }
+    console.log(balls)
+  }
+
+  const strikeHandle = (e) => {
+    e.preventDefault();
+    if(strikes < 3){
+      setStrikes(strikes + 1)
+    } else {
+      setStrikes(0)
+    }
+    console.log(strikes)
+  }
+
+  const foulHandle = (e) => {
+    e.preventDefault();
+    if(strikes === 0){
+      setStrikes(strikes + 1)
+    } else if (strikes === 1) {
+      setStrikes(strikes + 2)
+    } else {
+      setStrikes(strikes)
+    }
+    console.log(strikes)
+  }
+
+  const hitHandle = (e) => {
+    e.preventDefault();
+      setBalls(0);
+      setStrikes(0);
+      console.log(balls, strikes)
   }
 
 
   return (
     <div>
-      <button onClick={strike}>Strike</button>
-      <button onClick={ball}>Ball</button>
-      <button onClick={foul}>Foul</button>
-      <button onClick={hit}>Hit</button>
+      <button onClick={strikeHandle}>Strike</button>
+      <button onClick={ballHandle}>Ball</button>
+      <button onClick={foulHandle}>Foul</button>
+      <button onClick={hitHandle}>Hit</button>
     </div>
   );
 }
