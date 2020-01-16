3e23f63c4d2a64bd19461e6bea7e552b2b1cfa1b
Nhan <natenguyen51294@gmail.com>
2020-01-16 13:47:52 -0600

    basic set up components

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './Components/Dashboard';
+import Display from './Components/Display';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
